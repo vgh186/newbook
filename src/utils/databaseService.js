cba76bf3503515ece1920dadde9f48501c51dbd4
vgh186 <no-reply@github.com>
2025-04-29 17:06:19 +0800

    fix: remove duplicate getDbType import for Netlify build error

--- src/utils/databaseService.js
+++ src/utils/databaseService.js
@@ -2,7 +2,6 @@
 // 已移除 cloudbaseDB 相关内容
 import { getSupabaseClient } from './supabaseDB';
 import { getDbType } from './dbConfig';
-import { getDbType } from './dbConfig';
 
 // 统一的数据库服务
 // 已移除未使用的变量，确保 ESLint 通过
